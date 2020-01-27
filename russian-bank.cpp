b691a22785de023eedf88b6b954712c81ff05a6c
Olek <tupolewcx@gmail.com>
2020-01-27 21:04:01 +0100

    Budowa maina: działa wracanie karty na miejsce, czasami działa czzasami nie pobierane karty ze stosu, nie działa poprawne wyświetlanie karty przy ruszaniu myszką, następne kroki A) Bugfix powyrzszego B) Eventhandling kładzenie karty jedną na drugą

--- russian-bank.cpp
+++ russian-bank.cpp
@@ -28,7 +28,7 @@ int main()
             case sf::Event::MouseButtonPressed:
                 {
                     
-                    wherefrom_card_is_taken = [&]() -> Deck*
+                    wherefrom_card_is_taken = [&]() -> Deck* /*tu odbywa się walidacja skąd możemy zabrać karte*/ 
                     {
                         sf::Vector2f point (event.mouseButton.x,event.mouseButton.y);
 
@@ -66,8 +66,6 @@ int main()
                         wherefrom_card_is_taken->pop(); //usuwa karte ze stosu 
                         taken_card.value().setPosition(event.mouseButton.x,event.mouseButton.y);
                         
-
-                        //trzeba sprawdzić czy karta na pewno została zabrana bo nie mam pewności
                         if(wherefrom_card_is_taken->top()==taken_card.value()) throw std::runtime_error("Card obtaining Error");
                     }
                     
@@ -83,13 +81,49 @@ int main()
 				}
             case sf::Event::MouseButtonReleased:
                 {
+
                     if(taken_card.has_value() && wherefrom_card_is_taken!=nullptr)
                     {
-                        //weź karte
-                        //whereto_card_is_taken='wkaźnik do stosu na którym jest myszka'
-                        //konstruktor move
-                        //Move ruch(wherefrom_card_is_taken.value(), whereto_card_is_taken, std::move(taken_card.value()));
-                        //gra.players_move(gra.get_players_pointer(),ruch);
+                        //ustalamy wskaźnik do decku na miejsce gdzie jest kursor
+                        whereto_card_is_taken = [&]() ->Deck* /*tu NIE odbywa się walidacja dokąd możemy dać karte, sprawdzanie robie gra.players_move()*/
+                        {
+                            sf::Vector2f point (event.mouseButton.x,event.mouseButton.y);
+
+                            for(/*const*/Deck deck: gra.get_board().get_decks_arrays().first) //pola bankowe sprawdzamy
+                            {
+                                if(deck.get_rect().contains(point)) return &deck;
+                            }
+
+                            for(/*const*/Deck deck: gra.get_board().get_decks_arrays().second) //pola zew sprawdzamy
+                            {
+                                if(deck.get_rect().contains(point)) return &deck;//tu jest bug ale niew wiem jak go naprawić wherefrome_car_is_taken nie dostaje dobrego wskaźnika
+                            }
+
+                            if(gra.get_players_pointer()->get_deck_pointer()->get_rect().contains(point)) return gra.get_players_pointer()->get_deck_pointer();
+                            if(gra.get_players_pointer()->get_trash_pointer()->get_rect().contains(point)) return gra.get_players_pointer()->get_trash_pointer(); 
+                            if(gra.get_opponents_pointer()->get_deck_pointer()->get_rect().contains(point)) return gra.get_opponents_pointer()->get_deck_pointer();
+                            if(gra.get_opponents_pointer()->get_trash_pointer()->get_rect().contains(point)) return gra.get_opponents_pointer()->get_trash_pointer(); 
+
+                            //jeżeli gdzie kolwiek indziej klikneliśmy
+                            return nullptr;
+                        }();
+
+                        if(whereto_card_is_taken) //karta została położona na jakimś decku
+                        {
+                            //NIE ODPALAM TEGO BO POPRZEDNI KOD NIE DZIAŁA PRAWIDŁOWO
+                            //whereto_card_is_taken='wkaźnik do stosu na którym jest myszka'
+                            //konstruktor move
+                            //Move ruch(wherefrom_card_is_taken.value(), whereto_card_is_taken, std::move(taken_card.value()));
+                            //gra.players_move(gra.get_players_pointer(),ruch);
+                            //odłuż karte karte jeśli ruch się powiódł
+                        }
+                        else //karta została puszczona ale na złe nie na żaden deck, została położona na tle, cofamy ruch
+                        {
+                            wherefrom_card_is_taken->push(taken_card.value());
+                            taken_card.reset();
+                            wherefrom_card_is_taken=nullptr; //resetujemy bufory
+                        }
+                        
                     }
                     break;
                 }
