4463f075fdc3aa15fda3db2ca42da504049d2b25
hgonomeg <hgonomeg@gmail.com>
2020-02-27 15:01:18 +0100

    Develop enum logic of Cards

--- src/card.rs
+++ src/card.rs
@@ -1,4 +1,5 @@
 
+#[derive(Eq,PartialEq,PartialOrd,Ord)]
 pub enum CardValue {
     A,
     _2,
@@ -15,7 +16,7 @@ pub enum CardValue {
     K
 }
 
-
+#[derive(PartialEq,Eq)]
 pub enum CardColor {
     Hearts,
     Diamonds,
@@ -23,8 +24,45 @@ pub enum CardColor {
     Clubs //żołądź / trefl
 }
 
+impl CardColor {
+    pub fn is_red(&self) -> bool {
+        match self {
+            CardColor::Hearts => true,
+            CardColor::Diamonds => true,
+            _ => false
+        }
+    }
+    pub fn is_black(&self) -> bool {
+        !self.is_red()
+    }
+}
+
 
 pub struct Card {
     color : CardColor,
     value : CardValue
+}
+
+impl Card {
+    pub fn is_red(&self) -> bool {
+        self.color.is_red()
+    }
+    pub fn is_black(&self) -> bool {
+        self.color.is_black()
+    }
+    pub fn covers_ace_stack(&self,cov:&Card) -> bool {
+        if cov.color != self.color {
+            return false
+        }
+        self.value < cov.value
+    }
+    pub fn covers_king_stack(&self,cov:&Card) -> bool {
+        if cov.value < self.value || self.value == CardValue::A {
+            return false
+        }
+        match cov.is_red() {
+            true => self.is_black(),
+            false => self.is_red()
+        }
+    }
 }
